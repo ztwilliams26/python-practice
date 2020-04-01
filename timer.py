0c4a1472c6f7af25d58f2403e9fcf3e94bc04da4
ztwilliams26 <zwilliams6886@gmail.com>
2020-03-31 21:36:33 -0400

    Changed timer.py to practice using locks with threads

--- timer.py
+++ timer.py
@@ -1,19 +1,25 @@
-from threading import Thread
+import threading
 import time
+
+tLock = threading.Lock()
 
 
 def timer(name, delay, repeat):
     print("Time: "+name+" Started")
+    tLock.acquire()
+    print(name+" acquired thread lock")
     while repeat > 0:
         time.sleep(delay)
         print(name + ": "+str(time.ctime(time.time())))
         repeat -= 1
+    print(name+" is releasing thread lock")
+    tLock.release()
     print("timer: "+name+" is completed")
 
 
 def Main():
-    t1 = Thread(target=timer, args=("Timer1", 1, 5))
-    t2 = Thread(target=timer, args=("Timer2", 2, 3))
+    t1 = threading.Thread(target=timer, args=("Timer1", 1, 5))
+    t2 = threading.Thread(target=timer, args=("Timer2", 2, 3))
     t1.start()
     t2.start()
 
