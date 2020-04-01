af121d424c591d53868ccfaaf9352e4648a034d1
ztwilliams26 <zwilliams6886@gmail.com>
2020-03-31 23:31:59 -0400

    Database practice. test.db included in .gitignore file

---
+++ dbTest.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+
+def Main():
+    try:
+        con = sqlite3.connect('test.db')
+        cur = con.cursor()
+
+        cur.executescript("""
+                        DROP TABLE IF EXISTS Pets;
+                        CREATE TABLE Pets(Id INT, Name TEXT, Price INT);
+                        INSERT INTO Pets VALUES(1,"Cat", 400); 
+                        INSERT INTO Pets VALUES(2,"Dog", 600);  
+        """)
+
+        pets = (
+            (3, "Rabbit", 200),
+            (4, "Bird", 60),
+            (5, "Goad", 500)
+        )
+
+        cur.executemany("INSERT INTO Pets VALUES(?,?,?)", pets)
+        con.commit()
+
+        cur.execute('SELECT * FROM Pets WHERE Price<500')
+        data = cur.fetchall()
+
+        for row in data:
+            print(row)
+
+    except sqlite3.Error:
+        if con:
+            print("Error! Rolling back")
+            con.rollback()
+    finally:
+        if con:
+            con.close()
+
+
+if __name__ == '__main__':
+    Main()
